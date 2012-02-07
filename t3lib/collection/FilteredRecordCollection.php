097d460e753c47cbf8799a202cd8e10a2ed410a9
Oliver Hader <oliver@typo3.org>
2012-02-07 21:55:57 +0100

    [TASK] Clean-up and refactor collections
    Change-Id: I276496ae82924120620e598ef514ec29a9f60827

--- t3lib/collection/FilteredRecordCollection.php
+++ t3lib/collection/FilteredRecordCollection.php
@@ -1,5 +1,37 @@
 <?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2011-2012 Steffen Ritter <typo3@steffen-ritter.net>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
+/**
+ * Implements a record collection that is filtered by several criteria.
+ *
+ * @author Steffen Ritter <typo3@steffen-ritter.net>
+ * @package TYPO3
+ * @subpackage t3lib
+ */
 class t3lib_collection_FilteredRecordCollection extends t3lib_collection_AbstractRecordCollection {
 
 	/**
@@ -8,19 +40,33 @@ class t3lib_collection_FilteredRecordCollection extends t3lib_collection_Abstrac
 	 * @var string
 	 */
 	protected $criteria;
-	
+
+	/**
+	 * Initializes the object by using an array.
+	 *
+	 * @param array $array
+	 */
 	public function fromArray(array $array) {
 		parent::fromArray($array);
 		$this->setCriteria($array['criteria']);
 	}
 
+	/**
+	 * Gets the array representation of this object.
+	 *
+	 * @return array
+	 */
 	public function toArray() {
 		$array = parent::toArray();
 		$array['criteria'] = $this->getCriteria();
 		return $array;
 	}
 
-
+	/**
+	 * Gets the data array to be persisted.
+	 *
+	 * @return array
+	 */
 	protected function getPersistableDataArray() {
 		return array(
 			'title' => $this->getTitle(),
@@ -31,11 +77,18 @@ class t3lib_collection_FilteredRecordCollection extends t3lib_collection_Abstrac
 		);
 	}
 
+	/**
+	 * Loads the contents.
+	 *
+	 * @todo Implement loadContents() method.
+	 */
 	public function loadContents() {
-		// TODO: Implement loadContents() method.
+
 	}
 
 	/**
+	 * Sets the filter criteria.
+	 *
 	 * @param string $criteria
 	 */
 	public function setCriteria($criteria) {
@@ -43,10 +96,12 @@ class t3lib_collection_FilteredRecordCollection extends t3lib_collection_Abstrac
 	}
 
 	/**
+	 * Gets the filter criteria.
+	 *
 	 * @return string
 	 */
 	public function getCriteria() {
 		return $this->criteria;
 	}
-
 }
+?>
--- t3lib/collection/RecordCollectionRepository.php
+++ t3lib/collection/RecordCollectionRepository.php
@@ -1,6 +1,40 @@
 <?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2011-2012 Steffen Ritter <typo3@steffen-ritter.net>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
+/**
+ * Implements the repository for record collections.
+ *
+ * @author Steffen Ritter <typo3@steffen-ritter.net>
+ * @package TYPO3
+ * @subpackage t3lib
+ */
 class t3lib_collection_RecordCollectionRepository {
+	const TYPE_Static = 'static';
+	const TYPE_Filter = 'filter';
 
 	/**
 	 * the table name collections are stored to
@@ -10,68 +44,140 @@ class t3lib_collection_RecordCollectionRepository {
 	protected $table = 'sys_collection';
 
 	/**
-	 * @param $uid
-	 * @return void
+	 * Finds a record collection by uid.
+	 *
+	 * @param integer $uid
+	 * @return NULL|t3lib_collection_RecordCollection
 	 */
 	public function findByUid($uid) {
-		$data = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('uid', $this->table, 'uid = ' . intval($uid) . " " . t3lib_BEfunc::deleteClause('sys_collection'));
-		return $this->createDomainObject($data);
+		$result = NULL;
+
+		$data = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow(
+			'uid',
+			$this->table,
+			'uid = ' . intval($uid) . t3lib_BEfunc::deleteClause('sys_collection')
+		);
+
+		if ($data !== NULL) {
+			$result = $this->createDomainObject($data);
+		}
+
+		return $result;
 	}
 
-	public function findByRecordType($tableName) {
-		return $this->findByTypeAndRecord(NULL, $tableName);
+	/**
+	 * Finds record collections by table name.
+	 *
+	 * @param string $tableName Name of the table to query for
+	 * @return t3lib_collection_RecordCollection[]
+	 */
+	public function findByTableName($tableName) {
+		$conditions = array(
+			'table_name=' . $GLOBALS['TYPO3_DB']->fullQuoteStr($tableName, $this->table),
+		);
+
+		return $this->queryMultipleRecords($conditions);
 	}
 
+	/**
+	 * Finds record collection by type.
+	 *
+	 * @param $type
+	 * @return NULL|t3lib_collection_RecordCollection[]
+	 */
 	public function findByType($type) {
-		return $this->findByTypeAndRecord($type, NULL);
+		$conditions = array(
+			'type=' . $GLOBALS['TYPO3_DB']->fullQuoteStr($type, $this->table),
+		);
+
+		return $this->queryMultipleRecords($conditions);
 	}
 
-	public function findByTypeAndRecord($type, $tableName) {
+	/**
+	 * Finds record collections by type and table name.
+	 *
+	 * @param string $type
+	 * @param string $tableName
+	 * @return NULL|t3lib_collection_RecordCollection[]
+	 */
+	public function findByTypeAndTableName($type, $tableName) {
+		$conditions = array(
+			'type=' . $GLOBALS['TYPO3_DB']->fullQuoteStr($type, $this->table),
+			'table_name=' . $GLOBALS['TYPO3_DB']->fullQuoteStr($tableName, $this->table),
+		);
 
-		// @TODO: Check if we should escape the SQL below. Or have a positive list of types.
+		return $this->queryMultipleRecords($conditions);
+	}
 
-		$where = array();
-		if ($type !== NULL) {
-			$where[] = 'type = \'' . $type . "'";
-		}
+	/**
+	 * Deletes a record collection by uid.
+	 *
+	 * @param integer $uid
+	 */
+	public function deleteByUid($uid) {
+		$GLOBALS['TYPO3_DB']->exec_UPDATEquery(
+			$this->table, 'uid=' . intval($uid),
+			array('deleted' => 1, 'tstamp' => $GLOBALS['EXEC_TIME'])
+		);
+	}
 
-		if ($tableName !== NULL) {
-			$where[] = 'table_name = \'' . $tableName . "'";
-		}
+	/**
+	 * Queries for multiple records for the given conditions.
+	 *
+	 * @param array $conditions Conditions concatenated with AND for query
+	 * @return NULL|t3lib_collection_RecordCollection[]
+	 */
+	protected function queryMultipleRecords(array $conditions = array()) {
+		$result = NULL;
 
 		$data = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows(
 			'uid, type',
 			$this->table,
-			implode(' AND ', $where) . t3lib_BEfunc::deleteClause($this->table)
+			implode(' AND ', $conditions) . t3lib_BEfunc::deleteClause($this->table)
 		);
-		return $this->createManyDomainObjects($data);
-	}
 
-	public function deleteByUid($uid) {
-		$GLOBALS['TYPO3_DB']->exec_UPDATEquery($this->table, 'uid = ' . intval($uid), array('deleted' => 1, 'tstamp' => time()));
+		if ($data !== NULL) {
+			$result = $this->createMultipleDomainObjects($data);
+		}
+
+		return $result;
 	}
 
 	/**
+	 * Creates a record collection domain object.
+	 *
 	 * @param array $record
 	 * @return t3lib_collection_RecordCollection
 	 */
 	protected function createDomainObject(array $record) {
 		switch ($record['type']) {
-			case 'static':
+			case self::TYPE_Static:
 				$collection = t3lib_collection_StaticRecordCollection::load($record['uid']);
 				break;
-			case 'filter':
+			case self::TYPE_Filter:
 				$collection = t3lib_collection_FilteredRecordCollection::load($record['uid']);
 				break;
+			default:
+				throw new RuntimeException('Unknown record collection type "' . $record['type'], 1328646798);
 		}
+
 		return $collection;
 	}
 
-	protected function createManyDomainObjects(array $data) {
+	/**
+	 * Creates multiple record collection domain objects.
+	 *
+	 * @param array $data
+	 * @return t3lib_collection_RecordCollection[]
+	 */
+	protected function createMultipleDomainObjects(array $data) {
 		$collections = array();
-		foreach ($data AS $collection) {
+
+		foreach ($data as $collection) {
 			$collections[] = $this->createDomainObject($collection);
 		}
+
 		return $collections;
 	}
 }
+?>
